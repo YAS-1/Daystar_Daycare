62cd5e874c7d3b4481d6e36c6c0b6a73d379a8dc
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2025-04-16 11:57:38 +0300

    Rendering the Finances Page on Login

--- frontend/src/pages/ManagerDashboard.jsx
+++ frontend/src/pages/ManagerDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { React, useEffect } from "react";
 import { Link, Outlet, useNavigate, useLocation } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-toastify";
