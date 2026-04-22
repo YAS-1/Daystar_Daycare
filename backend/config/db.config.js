785067ea5c811b9d1f85796dd2f24b1df413a5c0
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2026-04-22 18:23:58 +0300

    Implement Docker setup and database connection logic; add Finances link to Manager Dashboard

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,16 +1,27 @@
 import mysql from "mysql2/promise";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = await mysql.createConnection({
-    host:  process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    port: process.env.PORT,
-    database: process.env.DATABASE
-});
+let db;
 
-console.log('Connected to MySQL database');
+const connectDB = async () => {
+  try {
+    db = await mysql.createConnection({
+      host: process.env.HOST,
+      user: process.env.USER,
+      password: process.env.PASSWORD,
+      port: process.env.PORT,
+      database: process.env.DATABASE,
+    });
+
+    console.log("Connected to MySQL database");
+  } catch (err) {
+    console.log("DB connection failed. Retrying in 5s...");
+    setTimeout(connectDB, 5000);
+  }
+};
+
+await connectDB();
 
 export default db;
